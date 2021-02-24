1973881d673affe0731589b74ed5f9a249454da8
Roberth Franco <roberthfranco16@gmail.com>
2021-02-23 20:12:06 -0500

    Create rulesandregulation.php

---
+++ Code/rulesandregulation.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Hotel Online Reservation</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+	</head>
+	
+<body>
+	
+	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+		<div
+		class = "container-fluid">
+			<div class = "navbar-header">
+				<a class = "navbar-brand" >Reserva en línea del hotel Iluminati</a>
+			</div>
+		</div>
+	</nav>	
+	<ul id = "menu">
+
+<li> <a href = "index.php"> Inicio </a> </li> |
+<li> <a href = "aboutus.php"> Sobre nosotros </a> </li> |
+<li> <a href = "contactus.php"> Contáctenos </a> </li> |
+<li> <a href = "gallery.php"> Galería </a> </li> |
+<li> <a href = "dineandlounge.php"> Dine and Lounge </a> </li> |
+<li> <a href = "reservation.php"> Hacer una reserva </a> </li> |
+<li> <a href= "rulesandregulation.php"> Reglas y regulación </a> </li>|
+
+<li> <a href= "google.com.ec"> Visita la pagina </a> </li>
+
+	</ul>
+	<div style = "margin-center:0;" class = "container">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+			
+				<strong><h3>REGLAS Y REGULACIÓN</h3></strong>
+				<br />
+			
+<strong> <h4 style = "color: #ff0000;"> 1. Tarifa </h4> </strong>
+<p> La tarifa es solo para la habitación y no incluye ningún impuesto gubernamental aplicable. Las comidas y otros servicios están disponibles por un costo adicional. Para conocer la tarifa de su habitación, comuníquese con el Gerente de Servicio, los formularios de registro de invitados deben estar firmados en las llegadas. </p>
+				<br />
+			
+<strong> <h4 style = "color: #ff0000;"> 2. Liquidación de facturas </h4> </strong>
+<p> Las facturas deben liquidarse en la presentación, no se aceptan cheques personales. </p>
+<br/>
+		
+<strong> <h4 style = "color: #ff0000;"> 3. Gravamen de la compañía sobre equipaje y pertenencias de los huéspedes </h4> </strong>
+<p> En caso de impago en el pago de las cuotas por parte de un huésped, la administración debe tener la ropa en sus maletas y pertenencias, y tener derecho a detenerlas y venderlas o subastarlas en cualquier momento sin hacer referencia al huésped. . El producto de la venta neta será apropiado para el monto adeudado por el huésped sin perjuicio de los derechos de la administración de adoptar tales procedimientos de recuperación adicionales cuando sea necesario. </P>
+<br/>
+		
+<strong> <h4 style = "color: #ff0000;"> 4. Check-in </h4> </strong>
+<p> Presente su tarjeta de identificación, pasaporte o tarjeta de residencia temporal al momento del check-in. Por ley, los visitantes deben presentar documentos personales para los registros del hotel. Estos documentos serán devueltos a la salida. </P>
+<br/>
+		
+<strong> <h4 style = "color: #ff0000;"> 5. Salida </h4> </strong>
+<p> La hora de salida es (mencione su hora de salida aquí) por favor informe a la recepción si desea retener su habitación más allá de este horario. La extensión se dará dependiendo de la disponibilidad. Si la habitación está disponible, se cobrará la tarifa normal. En caso de que el huésped no desocupe la habitación al vencimiento o período, la gerencia tendrá derecho a retirar al huésped y sus pertenencias de la habitación ocupada por el huésped. </P>
+<br/>
+			
+<strong> <h4 style = "color: #ff0000;"> 6. Almacenamiento de equipaje </h4> </strong>
+<p> Sujeto a la disponibilidad del espacio de almacenamiento, el huésped puede guardar el equipaje en la sala de equipaje, por cuenta y riesgo exclusivo del huésped, en caso de pérdida o daño por cualquier causa. El equipaje no puede almacenarse por más de 30 días. </p>
+<br/>
+			
+<strong> <h4 style = "color: #ff0000;"> 7. Mascotas </h4> </strong>
+<p> Mencione su política para Mascotas (permitidas o no permitidas) (Permítanos hacer arreglos por separado). </p>
+<br/>
+			
+<strong> <h4 style = "color: #ff0000;"> 8. Productos peligrosos </h4> </strong>
+<p> Está prohibido llevar productos y / o almacenar películas de cine en bruto o expuestas, o cualquier otro artículo de naturaleza combustible o peligrosa y / o productos prohibidos y / o productos de naturaleza objetable. </p>
+<br/>
+			
+<strong> <h4 style = "color: #ff0000;"> 9. Daño a la propiedad </h4> </strong>
+<p> El huésped será responsable de cualquier pérdida o daño a la propiedad del hotel causada por ellos mismos, sus invitados o cualquier persona de la que sean responsables. </p>
+<br/>
+				
+<strong> <h4 style = "color: #ff0000;"> 10. Derechos de la gerencia </h4> </strong>
+<p> Se acuerda que el huésped se comportará de manera respetable y no causará ninguna molestia o fastidio dentro de la premisa del hotel. </p>
+<br/>
+				
+<strong> <h4 style = "color: #ff0000;"> 11. Relación entre la dirección y el invitado </h4> </strong>
+<p> Nada de lo mencionado anteriormente continuará o se considerará que constituye, o crea cualquier tenencia o subarrendamiento, o cualquier otro derecho a interactuar en las instalaciones del hotel o parte de la misma, a favor de cualquier Huésped o residente o visitante, y la Dirección siempre se considerará en posesión plena y absoluta de la totalidad de las instalaciones del hotel. </p>
+<br/>
+				
+<strong> <h4 style = "color: #ff0000;"> 12. Reglas y regulaciones gubernamentales y aplicación de leyes ( Normativas de Bioseguridad )  </h4> </strong>
+<p> Los huéspedes deben observar, cumplir y aceptar todos los actos y leyes aplicables, así como las normas y regulaciones gubernamentales vigentes de vez en cuando. </p>
+<br/>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+	<div style = "text-align:right; margin-right:10px;" class = "navbar navbar-default navbar-fixed-bottom">
+		<label> </label>
+	</div>
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>	
+</html>
