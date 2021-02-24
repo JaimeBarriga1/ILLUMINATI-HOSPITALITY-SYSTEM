c23bd1769f6c01869a92ed153c5cf964b492f4e6
Roberth Franco <roberthfranco16@gmail.com>
2021-02-23 20:12:27 -0500

    Create reply_reserve.php

---
+++ reply_reserve.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>SistHot</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+	</head>
+<body>
+	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+		<div  class = "container-fluid">
+			<div class = "navbar-header">
+				<a class = "navbar-brand" >Reserva en línea del hotel</a>
+			</div>
+		</div>
+	</nav>	
+	<ul id = "menu">
+	<li> <a href = "index.php"> Inicio </a> </li> |
+<li> <a href = "aboutus.php"> Sobre nosotros </a> </li> |
+<li> <a href = "contactus.php"> Contáctenos </a> </li> |
+<li> <a href = "gallery.php"> Galería </a> </li> |
+<li> <a href = "dineandlounge.php"> Dine and Lounge </a> </li> |
+<li> <a href = "reservation.php"> Hacer una reserva </a> </li> |
+<li> <a href= "rulesandregulation.php"> Reglas y regulación </a> </li>
+
+<li> <a href= > </a> </li>
+	</ul>
+	<div style = "margin-left:0;" class = "container">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<strong><h3>HACER UNA RESERVA</h3></strong>
+				<br />
+				<div class = "col-md-4"></div>
+				<div class = "well col-md-4">
+					<center><h3>Visita nuestro Hotel para verificar</h3></center>
+					<br />
+					<center><h4>GRACIAS!</h4></center>
+					<br />
+					<center><a href = "reservation.php" class = "btn btn-success"><i class = "glphyicon glyphicon-check"></i> Volver a la reserva</a></center>
+				</div>
+				<div class = "col-md-4"></div>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+	<div style = "text-align:right; margin-right:10px;" class = "navbar navbar-default navbar-fixed-bottom">
+		<label> </label>
+	</div>
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>	
+</html>
